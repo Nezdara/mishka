d5fb8ff7128dcf6f1508d0fb7fa2b0e5f001b0c8
Nezdara <nezdara@gmail.com>
2016-11-07 12:57:31 +0200

    interactive map

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+function initMap() {
+  var mapOptions = {
+    zoom: 16,
+    center: {lat: 59.939116, lng: 30.322412},
+    disableDefaultUI: true
+  }
+
+  var map = new google.maps.Map(document.querySelector('.contacts__map'), mapOptions);
+  var image = "img/icon-map-pin.svg";
+  var myLatLng = new google.maps.LatLng(59.939116, 30.322412);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image,
+    animation: google.maps.Animation.DROP
+  });
+}
